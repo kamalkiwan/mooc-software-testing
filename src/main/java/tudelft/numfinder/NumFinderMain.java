ab0c496318add3c2a68afcee593af506547650f0
kamalkiwan <kamalkiwan@gmail.com>
2021-06-21 02:55:30 +0200

    fix findnum

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -5,8 +5,8 @@ public class NumFinderMain {
     public static void main (String[] args) {
         NumFinder nf = new NumFinder();
 
-        // this works
-        // nf.find(new int[] {4, 25, 7, 9});
+         // this works
+         // nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
         nf.find(new int[] {4, 3, 2, 1});
