746ce99a7be17da0bcf394aeb234673496a55903
kamalkiwan <kamalkiwan@gmail.com>
2021-06-21 05:13:37 +0200

    add test to caesershiftcipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,26 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
+    private CaesarShiftCipher csc;
+
+    @BeforeEach
+    public void initailize (){
+        this.csc = new CaesarShiftCipher();
+    }
+    @Test
+    public void normalMessageABC(){
+        String result = "def";
+        Assertions.assertEquals(csc.CaesarShiftCipher("abc",    3),result );
+    }
+
+    @Test
+    public void normalMessageXYZ(){
+        String result = "abc";
+        Assertions.assertEquals(csc.CaesarShiftCipher("xyz",    3),result );
+    }
 
 }
