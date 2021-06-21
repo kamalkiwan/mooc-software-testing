88dfd57694e788ff5144e911e955f756fa35ce69
kamalkiwan <kamalkiwan@gmail.com>
2021-06-21 04:59:01 +0200

    add tests to GHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,29 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    private GHappy gh;
+
+    @BeforeEach
+    public void initialize (){this.gh = new GHappy();}
+    @Test
+    public void GHappyReturnsTrue(){
+        boolean result = gh.gHappy("hhhgghhh");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void GHappyReturnsFlaseWithOneG(){
+        boolean result = gh.gHappy("xxgxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void GHappyReturnsFalseWitEvenNumberOfG(){
+        boolean result = gh.gHappy("xxggyygxx");
+        Assertions.assertFalse(result);
+    }
 }
